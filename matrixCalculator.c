f57c858f0e69e44341bfa08203813edae2f3a543
Ryan <kinger145@gmail.com>
2017-12-28 18:14:44 -0500

    Product function implemented

--- matrixCalculator.c
+++ matrixCalculator.c
@@ -13,6 +13,7 @@ main()
   int crr[2][2];
   printf("%d\n", getRank(2, 2, arr));
   getDifference(2, 2, arr, brr, crr);
+  getProduct(2, 2, arr, 2, 2, brr);
   printMatrix(2, 2, crr);
 }
 
@@ -35,9 +36,32 @@ int getRank(size_t rows, size_t cols, int matrix[rows][cols])
   return rank;
 }
 
-void getProduct(size_t rows, size_t cols, int matrix[rows][cols], size_t rows2, size_t cols2, int bmatrix[rows2][cols2])
+//works
+void getProduct(size_t rows, size_t cols, int aMatrix[rows][cols], size_t rows2, size_t cols2, int bMatrix[rows2][cols2])
 {
+    if(cols != rows2)
+    {
+      return;
+    }
 
+    int pMatrix[rows][cols2];
+    int mark = 0;
+
+    for(int i = 0; i < rows; i++)
+    {
+      for(int j = 0; j < cols2; j++)
+      {
+          int sum = 0;
+          for(int k = 0; k < cols; k++)
+          {
+            sum += aMatrix[i][k] * bMatrix[k][j];
+          }
+
+          pMatrix[i][j] = sum;
+      }
+    }
+
+    printMatrix(rows, cols2, pMatrix);
 }
 
 //works
@@ -64,6 +88,21 @@ void getDifference(size_t rows, size_t cols, int matrix[rows][cols], int matrix2
   }
 }
 
+void getREF(size_t rows, size_t cols, int matrix[rows][cols])
+{
+
+}
+
+/**
+ * void getInverse(size_t rows, size_t cols, int matrix[rows][cols])
+ * {
+ *  //calculate the matrix of minors
+ *  //then turn that into the matrix of cofactors
+ *  //then the adjugate
+ *  //multiply that by 1/determinant
+ * }
+  */
+  
 /**
  * Helper function to print a matrix.
  */
--- matrixCalculator.h
+++ matrixCalculator.h
@@ -1,6 +1,7 @@
 int getRank(size_t rows, size_t cols, int matrix[rows][cols]);
-void getProduct(size_t rows, size_t cols, int matrix[rows][cols], size_t rows2, size_t cols2, int bmatrix[rows2][cols2]);
+void getProduct(size_t rows, size_t cols, int aMatrix[rows][cols], size_t rows2, size_t cols2, int bMatrix[rows2][cols2]);
 void getSum(size_t rows, size_t cols, int matrix[rows][cols], int matrix2[rows][cols], int matrix3[rows][cols]);
 void getDifference(size_t rows, size_t cols, int matrix[rows][cols], int matrix2[rows][cols], int matrix3[rows][cols]);
 void transpose(size_t rows, size_t cols, int **matrix[rows][cols]);
+void getREF(size_t rows, size_t cols, int matrix[rows][cols]);
 void printMatrix(size_t rows, size_t cols, int matrix[rows][cols]);
